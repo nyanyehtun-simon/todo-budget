905846920df9fadbefa1e9bed1ab06d6c2adc20e
Nyanye htun <nyanyehtun@my.jcu.edu.au>
2019-08-26 17:30:00 +0800

    Refactor code to comply with typescript

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,72 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  // @ViewChild('inputReason') reasonInput: ElementRef;
+  // @ViewChild('inputAmount') amountInput: ElementRef;
+  // @ViewChild('expensesList') expensesList: ElementRef;
+  // @ViewChild('totalExpenses') totalExpensesOutput: ElementRef;
+  // @ViewChild('expensesList') expensesList: ElementRef;
 
+  public reasonInput:string;
+  public amountInput: string;
+  public totalExpenses = 0;
+
+  public expensesListData = {}
+  public expensesListKeys = []
+
+  constructor(private alertCtrl: AlertController) {}
+
+  public clear() {
+    this.reasonInput = "";
+    this.amountInput = "";
+  }
+
+  public clickConfirmButton(e: Event) {
+    console.log(this.reasonInput)
+    console.log(this.amountInput)
+    const enteredReason = this.reasonInput;
+    const enteredAmount = this.amountInput;
+
+    if (
+      enteredReason.trim().length <= 0 ||
+      enteredAmount <= 0 ||
+      enteredAmount.trim().length <= 0
+    ) {
+      alertCtrl
+        .create({
+          message: "Please enter valid values",
+          header: "Input error",
+          buttons: ["Okay"]
+        })
+        .then(alertElement => {
+          alertElement.present();
+        });
+      console.log("\n %c!!INPUT ERROR!!", "color: red; font-size:15px;");
+      return;
+    }
+
+    const newItem = document.createElement("ion-button");
+    newItem.color = "warning";
+    newItem.fill = "solid";
+    newItem.textContent = enteredReason + ": $" + enteredAmount;
+
+    this.expensesListData[enteredReason] = enteredAmount;
+
+    this.expensesListKeys.push(enteredReason);
+
+    // this.expensesList = Object.assign()
+    console.log(this.expensesListData);
+
+    // this.expensesList.appendChild(newItem);
+    console.log("Reason:", enteredReason, "|| Amount:", enteredAmount, "$");
+
+    this.totalExpenses += parseInt(enteredAmount);
+    // console.log("his total", totalExpenses);
+    // totalExpensesOutput.textContent = +(
+    //   Math.round(totalExpenses + "e+2") + "e-2"
+    // );
+
+    this.clear();
   }
 
 }
